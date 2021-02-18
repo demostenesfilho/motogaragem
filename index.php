c70d28697433dff94c8a17616eb2041a3611358b
Demóstenes Filho <58401504+demostenesfilho@users.noreply.github.com>
2021-02-18 17:57:59 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -22,11 +22,22 @@
               <a href="#name"><span class="white-text name">Demóstenes Pedrosa</span></a>
               <a href="#email"><span class="white-text email">motogaragem.caruaru@gmail.com</span></a>
             </div></li>
-            <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-            <li><a href="#!">Second Link</a></li>
+            <li><a href="#!"><i class="material-icons">home</i>Início</a></li>
             <li><div class="divider"></div></li>
-            <li><a class="subheader">Subheader</a></li>
-            <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+            <li><a class="subheader">Atendimentos</a></li>
+            <li><a href="#!"><i class="material-icons">assignment</i>Agendamentos</a></li>
+            <li><a href="#!"><i class="material-icons">assignment_late</i>Em atendimento</a></li>
+            <li><a href="#!"><i class="material-icons">assignment_turned_in</i>Concluídos</a></li>
+            <li><div class="divider"></div></li>
+            <li><a class="subheader">Vendas</a></li>
+            <li><a href="#!"><i class="material-icons">construction</i>Peças</a></li>
+            <li><a href="#!"><i class="material-icons">bolt</i>Baterias</a></li>
+            <li><a href="#!"><i class="material-icons">science</i>Troca de óleos</a></li>
+            <li><div class="divider"></div></li>
+            <li><a class="subheader">Dados úteis</a></li>
+            <li><a href="#!"><i class="material-icons">person</i>Clientes</a></li>
+            <li><a href="#!"><i class="material-icons">two_wheeler</i>Motos</a></li>
+
           </ul>
                 
 
